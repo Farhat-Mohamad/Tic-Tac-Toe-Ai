8b01d2253daae112b3ba4e7d212ae54b7f1ec45d
Mohamad Farhat <mohamadfarhatt7@gmail.com>
2025-09-28 17:42:44 +0330

    no_winner function is done. minimax function algorithm is done.

--- 0.py
+++ 0.py
@@ -1,12 +1,16 @@
 EMPTY = ' '
 
-map = [[EMPTY for _ in range(3)] for _ in range (3)]
+map = [
+    [EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY],
+]
 
 def printMap(a):
     for b in a:
         print(b)
 
-def is_a_winner(a, player):
+def check_winner(a, player):
     if (a[0][0]=='player' and a[0][1]=='player' and a[0][2]=='player') or ( 
     a[1][0]=='player' and a[1][1]=='player' and a[1][2]=='player') or (
     a[2][0]=='player' and a[2][1]=='player' and a[2][2]=='player') or (
@@ -19,39 +23,42 @@ def is_a_winner(a, player):
     else:
         return False
     
+def no_winner(a):
+    if a[0][0]!=EMPTY and a[0][1]!=EMPTY and a[0][2]!=EMPTY and a[1][0]!=EMPTY and a[1][1]!=EMPTY and a[1][2]!=EMPTY and a[2][0]!=EMPTY and a[2][1]!=EMPTY and a[2][2]!=EMPTY and check_winner(map, "X")==False and check_winner(map, "O")==False :
+        return True
+    return False
+
+    
+    
 def minimax(map, isMaxing, depth):
+    if check_winner(map,"O") == True:
+        return 1
+    elif check_winner(map,"X") == True:
+        return -1
+    elif no_winner(map)==True or depth ==0:
+        return 0
     
+    if isMaxing:
+        value = ('-Infinity')
+        for i in range(3):
+            for j in range(3):
+                if map[i][j] == EMPTY:
+                    map[i][j] = "O"
+                    other_value = minimax(map, False, depth-1)
+                    value = max(value, other_value)
+                    map[i][j] = EMPTY
+        return value
     
-printMap(map)
+    if not isMaxing:
+        value = ('+Infinity')
+        for i in range(3):
+            for j in range(3):
+                if map[i][j] == EMPTY:
+                    map[i][j] = "X"
+                    other_value = minimax(map, True, depth-1)
+                    value = min(value, other_value)
+                    map[i][j]= EMPTY
+        return value
 
-while( O_is_winner(map) == False and X_is_winner(map) == False):
-        
-    print("X's Turn\nEnter your coordinates: ")
-    x = int(input())
-    y = int(input())
 
-    while x < 0 or y < 0 or x > 2 or y > 2 or map[x][y] != EMPTY or map[x][y] == 'O':
-        print("Invalid movement, try again:")
-        x = int(input())
-        y = int(input())
-    map[x][y] = 'X'
-    printMap(map)
 
-    if X_is_winner(map) == True:
-        print("The player X is the winner!!")
-        break
-
-    print("O's Turn\nEnter your coordinates: ")
-    x = int(input())
-    y = int(input())
-
-    while x < 0 or y < 0 or x > 2 or y > 2 or map[x][y] != EMPTY or map[x][y] == 'X':
-        print("Invalid move, try again")
-        x = int(input())
-        y = int(input())
-    map[x][y] = 'O'
-    printMap(map)
-
-    if O_is_winner(map) == True:
-        print("The player O is the winner!!")
-        break
