9add2a20921a1a9ba922074a6b49b2944b8703d8
Mohamad Farhat <mohamadfarhatt7@gmail.com>
2025-09-30 16:45:14 +0330

    Labeling small boxes is done

--- 0.1.py
+++ 0.1.py
@@ -7,6 +7,7 @@ def printMap(a):
     for i in range(9):
         cell = " | ".join(a[i])
         print (cell)
+    print("\n")
 
 
 def box1_winner(a, player):
@@ -124,52 +125,45 @@ def game_winner(a,player):
 
 printMap(map)
 
-#def X_is_winner(a):
-    # if (a[0][0]=='X' and a[0][1]=='X' and a[0][2]=='X') or ( 
-    # a[1][0]=='X' and a[1][1]=='X' and a[1][2]=='X') or (
-    # a[2][0]=='X' and a[2][1]=='X' and a[2][2]=='X') or (
-    # a[0][0]=='X' and a[1][0]=='X' and a[2][0]=='X') or (
-    # a[0][1]=='X' and a[1][1]=='X' and a[2][1]=='X') or (
-    # a[0][2]=='X' and a[1][2]=='X' and a[2][2]=='X') or (
-    # a[0][0]=='X' and a[1][1]=='X' and a[2][2]=='X') or (
-    # a[0][2]=='X' and a[1][1]=='X' and a[2][0]=='X'):
-    #     return True
-#     else:
-#         return False
-
-
-# map = [[EMPTY for _ in range(3)] for _ in range (3)]
+   
+def map_is_full(a):
+    for i in range(9):
+        for j in range(9):
+            if a[i][j] == EMPTY:
+                return False
+    return True
     
-# printMap(map)
+    
+def boxes_number(a):
+    label = 0
+    for i in range(6,9):
+        for j in range(9):
+            if (j>= 6 and j<9):
+                label = 9
+            if (j>= 3 and j<6):
+                label = 8
+            if (j<3):
+                label = 7
+    for i in range(3,6):
+        for j in range(9):
+            if (j>= 6 and j<9):
+                label = 6
+            if (j>= 3 and j<6):
+                label = 5
+            if (j<3):
+                label = 4
+    for i in range(3):
+        for j in range(9):
+            if (j>= 6 and j<9):
+                label = 3
+            if (j>= 3 and j<6):
+                label = 2
+            if (j<3):
+                label = 1    
+    
+            
+            
+             
+            
 
-# while( O_is_winner(map) == False and X_is_winner(map) == False):
-        
-#     print("X's Turn\nEnter your coordinates: ")
-#     x = int(input())
-#     y = int(input())
-
-#     while x < 0 or y < 0 or x > 2 or y > 2 or map[x][y] != EMPTY or map[x][y] == 'O':
-#         print("Invalid movement, try again:")
-#         x = int(input())
-#         y = int(input())
-#     map[x][y] = 'X'
-#     printMap(map)
-
-#     if X_is_winner(map) == True:
-#         print("The player X is the winner!!")
-#         break
-
-#     print("O's Turn\nEnter your coordinates: ")
-#     x = int(input())
-#     y = int(input())
-
-#     while x < 0 or y < 0 or x > 2 or y > 2 or map[x][y] != EMPTY or map[x][y] == 'X':
-#         print("Invalid move, try again")
-#         x = int(input())
-#         y = int(input())
-#     map[x][y] = 'O'
-#     printMap(map)
-
-#     if O_is_winner(map) == True:
-#         print("The player O is the winner!!")
-#         break
+ 
